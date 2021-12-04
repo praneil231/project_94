a1e80d778225a58f9d1860d796cbc91972cef0f7
praneil231 <84107249+praneil231@users.noreply.github.com>
2021-12-04 11:30:52 -0500

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,5 +1,5 @@
 function addUser(){
-    user_name=document.getElementById("user_name").value;
-    //add code to save in local storage 
-    window.location.replace("kwitter_room.html");
+    username=document.getElementById("user_name").value;
+    localStorage.setItem("user_name",username);
+    window.location.assign("/kwitter_room.html");
 };
